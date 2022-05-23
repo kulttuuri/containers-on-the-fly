5c575cb137e1bda823e71af6ad410895e26baef5
Aleksi Postari <riialin@gmail.com>
2022-05-23 14:05:49 +0300

    - Added base template for the admin cli.

---
+++ webapp/backend/admincli.py
@@ -0,0 +1,25 @@
+from helpers.admin import *
+
+print("Welcome to AI Reservation Server Admin CLI")
+
+def main():
+  print("What do you want to manage?")
+  print("You can manage: users, whitelisting, containers, hardwarespecs, reservations, roles")
+  print("Type exit to exit the program")
+  selection = input()
+  if (selection == "users"): users()
+
+def users():
+  print("Managing users")
+  print("What  do you want to do?")
+  print("1) List all users")
+    # 1) List all users
+    # 2) List users email containing
+    # 3) Go back
+  print("2) Add new user")
+  print("3) Remove user")
+  print("4) Edit user")
+  print("5) Go back")
+  selection = input()
+
+main()
