d4d1d615f1a1440067c25f79b04459a97d0c1cae
Aleksi Postari <aleksi.postari@samk.fi>
2024-05-21 19:35:25 +0300

    Bind the fastapi server in IP 0.0.0.0 to prevent any launch problems.

--- webapp/backend/main.py
+++ webapp/backend/main.py
@@ -35,5 +35,5 @@ if __name__ == '__main__':
     reload = True
     if production == True: logLevel = "critical"
     if production == True: reload = False
-    uvicorn.run("main:app", host=settings.app["host"], port=int(settings.app["port"]), log_level=logLevel, reload=reload)
+    uvicorn.run("main:app", host="0.0.0.0", port=int(settings.app["port"]), log_level=logLevel, reload=reload)
     print("running")
