b96a7eb7b369dd9916b696f2a79bba1fb3d1ca92
Aleksi Postari <aleksi.postari@samk.fi>
2024-05-24 10:23:28 +0300

    Remove merge settings making backup .bak files.

--- scripts/merge_settings.bash
+++ scripts/merge_settings.bash
@@ -17,24 +17,24 @@ escape_sed() {
 
 # Timezone
 ESCAPED_TIMEZONE=$(escape_sed "$TIMEZONE")
-sed -i.bak "s/^\([[:space:]]*\"timezone\": \).*/\1\"$ESCAPED_TIMEZONE\",/" user_config/backend_settings.json
-sed -i.bak "s/^\([[:space:]]*timezone: \).*/\1\"$ESCAPED_TIMEZONE\",/" user_config/frontend_settings.js
+sed -i "s/^\([[:space:]]*\"timezone\": \).*/\1\"$ESCAPED_TIMEZONE\",/" user_config/backend_settings.json
+sed -i "s/^\([[:space:]]*timezone: \).*/\1\"$ESCAPED_TIMEZONE\",/" user_config/frontend_settings.js
 
 # Server address in frontend settings file
 ESCAPED_SERVER_WEB_ADDRESS=$(escape_sed "$SERVER_WEB_ADDRESS")
-sed -i.bak "s/^\([[:space:]]*baseAddress: \).*/\1\"$ESCAPED_SERVER_WEB_ADDRESS\/api\/\",/" user_config/frontend_settings.js
+sed -i "s/^\([[:space:]]*baseAddress: \).*/\1\"$ESCAPED_SERVER_WEB_ADDRESS\/api\/\",/" user_config/frontend_settings.js
 
 # App name
 ESCAPED_APP_NAME=$(escape_sed "$APP_NAME")
-sed -i.bak "s/^\([[:space:]]*\"name\": \).*/\1\"$APP_NAME\",/" user_config/backend_settings.json
-sed -i.bak "s/^\([[:space:]]*appName: \).*/\1\"$APP_NAME\",/" user_config/frontend_settings.js
+sed -i "s/^\([[:space:]]*\"name\": \).*/\1\"$APP_NAME\",/" user_config/backend_settings.json
+sed -i "s/^\([[:space:]]*appName: \).*/\1\"$APP_NAME\",/" user_config/frontend_settings.js
 
 # Email address
 ESCAPED_EMAIL_ADDRESS=$(escape_sed "$CONTACT_EMAIL")
-sed -i.bak "s/^\([[:space:]]*\"helpEmailAddress\": \).*/\1\"$ESCAPED_EMAIL_ADDRESS\"/" user_config/backend_settings.json
-sed -i.bak "s/^\([[:space:]]*contactEmail: \).*/\1\"$ESCAPED_EMAIL_ADDRESS\",/" user_config/frontend_settings.js
+sed -i "s/^\([[:space:]]*\"helpEmailAddress\": \).*/\1\"$ESCAPED_EMAIL_ADDRESS\"/" user_config/backend_settings.json
+sed -i "s/^\([[:space:]]*contactEmail: \).*/\1\"$ESCAPED_EMAIL_ADDRESS\",/" user_config/frontend_settings.js
 
 # Database URI
 URI="mysql+pymysql://"$MARIADB_DB_USER":"$MARIADB_DB_USER_PASSWORD"@"$MARIADB_SERVER_ADDRESS"/"$MARIADB_DB_NAME
 ESCAPED_URI=$(escape_sed "$URI")
-sed -i.bak "s/^\([[:space:]]*\"engineUri\": \).*/\1\"$ESCAPED_URI\",/" user_config/backend_settings.json
+sed -i "s/^\([[:space:]]*\"engineUri\": \).*/\1\"$ESCAPED_URI\",/" user_config/backend_settings.json
