d97a1d1ec8315cdbd36faa95d6a1f4e77077fdea
Aleksi Postari <riialin@gmail.com>
2024-10-25 10:32:50 +0300

    Add backend endpoints and frontend components for managing groups
    **Backend**
    * Add `adminGroups.py` to define endpoints for managing groups
    * Include endpoints for adding, removing, editing groups, and managing group members
    * Use `ForceAuthentication` and `CheckIp` for security checks
    
    **Frontend**
    * Add `AdminGroupsTable.vue` to create a table component for displaying groups
    * Add `AdminManageGroupModal.vue` to create a modal component for managing groups
    * Add `PageAdminGroups.vue` to create a page component for managing groups
    * Add `ViewAdminGroups.vue` to create a view component for the groups management page
    * Modify `router.js` to add a new route for the groups management page

---
+++ webapp/frontend/src/components/admin/AdminGroupsTable.vue
@@ -0,0 +1,94 @@
+<template>
+  <div>
+    <a v-if="hasLongItems" class="link-toggle-read-all" @click="toggleReadAll">{{ !readAll ? "Read all" : "Read less" }}</a>
+    <v-data-table
+      :headers="table.headers"
+      :items="data"
+      :sort-by="'groupId'"
+      :sort-desc="true"
+      class="elevation-1">
+
+      <!-- Actions -->
+      <template v-slot:item.actions="{item}">
+        <a class="link-action" @click="emitEditGroup(item.groupId)">Edit Group</a>
+        <a class="link-action" @click="emitRemoveGroup(item.groupId)">Remove Group</a>
+      </template>
+    </v-data-table>
+  </div>
+</template>
+
+<script>
+  import { DisplayTime } from '/src/helpers/time.js'
+
+  export default {
+    name: 'AdminGroupsTable',
+    props: {
+      propItems: {
+        type: Array,
+        required: true,
+      }
+    },
+    data: () => ({
+      data: [],
+      readAll: false,
+      hasLongItems: false,
+      table: {
+        headers: [
+          { text: 'Group ID', value: 'groupId' },
+          { text: 'Name', value: 'name' },
+          { text: 'Created At', value: 'createdAt' },
+          { text: 'Updated At', value: 'updatedAt' },
+          { text: 'Actions', value: 'actions' },
+        ],
+      }
+    }),
+    mounted () {
+      this.data = this.propItems
+    },
+    methods: {
+      emitEditGroup(groupId) {
+        this.$emit('emitEditGroup', groupId)
+      },
+      emitRemoveGroup(groupId) {
+        this.$emit('emitRemoveGroup', groupId)
+      },
+      toggleReadAll() {
+        this.readAll = !this.readAll;
+      },
+      getText(text) {
+        if (this.readAll) return text;
+        else {
+          if (!this.hasLongItems) this.hasLongItems = true;
+          return text.slice(0,10) + "...";
+        }
+      },
+      parseTime(timestamp) {
+        return DisplayTime(timestamp)
+      },
+    },
+    watch: {
+      propItems: {
+        handler(newVal) {
+          this.data = newVal
+        },
+        immediate: true,
+      },
+    },
+  }
+</script>
+
+<style scoped lang="scss">
+  .link-action {
+    display: block;
+    min-width: 150px;
+    margin: 10px 0px;
+  }
+
+  .link-toggle-read-all {
+    margin-bottom: 20px;
+    font-size: 14px;
+    display: inline-block;
+    padding-left: 15px;
+    width: auto;
+  }
+</style>
